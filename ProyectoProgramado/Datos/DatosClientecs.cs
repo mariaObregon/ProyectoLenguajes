6d94423c0b16c192b178688ece9c73db1b4d6716
Piedra <Piedra@DESKTOP-MMAIBSU>
2018-06-12 23:59:59 -0600

    Apenas la base de los platos

--- ProyectoProgramado/Datos/DatosClientecs.cs
+++ ProyectoProgramado/Datos/DatosClientecs.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Datos
 {
-    public class Class1
+    class DatosClientecs
     {
     }
 }
--- ProyectoProgramado/Datos/DatosUsuario.cs
+++ ProyectoProgramado/Datos/DatosUsuario.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entidad
+namespace Datos
 {
-    public class Class1
+    class DatosUsuario
     {
     }
 }
