d6367ff2433fb711c2233a604e6382dddfa94829
Julien King <kingJulien2397@hotmail.com>
2018-06-26 08:36:33 -0600

    Ya funciona para usar scipt y update

---
+++ ProyectoProgramado/Presentacion/InsertarPlato.aspx.cs
@@ -0,0 +1,27 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Presentacion
+{
+    public partial class InsertarPlato1 : System.Web.UI.Page
+    {
+        LogicaPlato lp = new LogicaPlato();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void butGuardarPlato_Click(object sender, EventArgs e)
+        {
+            byte[] newB = lp.ConvertirImagenBinario(InsertarImagenPlato);
+
+            lp.AgregarPlato(txtNombrePlato.Text, txtDescPlato.Text, Decimal.Parse(txtPrecioPlato.Text), newB, lp.EstadoHabilitado(DropDownList1.SelectedValue));
+        }
+    }
+}
