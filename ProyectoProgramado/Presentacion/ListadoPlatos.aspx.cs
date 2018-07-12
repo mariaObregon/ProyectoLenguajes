0a2b4d5679a938e94a92fd5ace092ddbc9bf6279
Julien King <kingJulien2397@hotmail.com>
2018-07-11 19:28:53 -0600

    .

--- ProyectoProgramado/Presentacion/ListadoPlatos.aspx.cs
+++ ProyectoProgramado/Presentacion/ListadoPlatos.aspx.cs
@@ -11,14 +11,21 @@ namespace Presentacion
 {
     public partial class ListadoPlatos : System.Web.UI.Page
     {
-        List<f_buscarPlatoID_Result> platosCarrito; // new List<f_buscarPlatoID_Result>();
+        List<f_buscarPlatoID_Result> carrito; // new List<f_buscarPlatoID_Result>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            carrito = (List<f_buscarPlatoID_Result>)Session["carritoPlatos"];
 
-            if (true)
+            if (carrito == null)
             {
+                //Poner un msj de que no hay platos agregados a la lista para realizar el pedido
 
+            }
+            else
+            {
+                gridPlatosAgregados.DataSource = carrito;
+                gridPlatosAgregados.DataBind();
             }
 
         }
